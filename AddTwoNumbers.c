d87fd92ecaa9d9c8be6e26582bd51f8dc1d24658
siam-parvez <siamparvez44@gmail.com>
2026-06-20 07:53:50 +0600

    Addition program written

---
+++ AddTwoNumbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+// 41260103268
+{
+    int num1, num2;
+
+    printf("Insert First Number: ");
+    scanf("%d", &num1);
+
+    printf("Insert Second Number: ");
+    scanf("%d", &num2);
+
+    int sum = num1 + num2;
+
+    printf("The addition result is: %d", sum);
+
+    return 0;
+}
